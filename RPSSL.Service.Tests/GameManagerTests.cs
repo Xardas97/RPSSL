00ba794e8f26d72597e6101dc985ab622c663b28
Xardas97 <markomicovic19@yahoo.com>
2024-10-14 11:02:41 +0200

    Add Unit Tests with xUnit

---
+++ RPSSL.Service.Tests/GameManagerTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+
+using Moq;
+using Xunit;
+
+using Mmicovic.RPSSL.Service.Models;
+
+namespace Mmicovic.RPSSL.Service.Tests
+{
+    public class GameManagerTests
+    {
+        private const int MAX_SHAPE_ID = 10;
+        private const int PLAYER_SHAPE_ID = 4;
+        private const int GENERATED_SHAPE_ID = 7;
+        private const string GENERATED_SHAPE_NAME = "GeneratedShape";
+        private static readonly Shape GENERATED_SHAPE = new(GENERATED_SHAPE_ID, GENERATED_SHAPE_NAME);
+
+        private readonly Mock<IShapeProvider> shapeProvider;
+        private readonly Mock<IRandomGenerator> randomGenerator;
+        private readonly Mock<IGameResultCalculator> gameResultCalculator;
+
+        private readonly IGameManager gameManager;
+
+        public GameManagerTests()
+        {
+            shapeProvider = new Mock<IShapeProvider>();
+            randomGenerator = new Mock<IRandomGenerator>();
+            gameResultCalculator = new Mock<IGameResultCalculator>();
+
+            var logger = new Mock<ILogger<GameManager>>();
+            gameManager = new GameManager(randomGenerator.Object, gameResultCalculator.Object,
+                                          shapeProvider.Object, logger.Object);
+        }
+
+        [Theory]
+        [InlineData(Result.Tie)]
+        [InlineData(Result.Lose)]
+        [InlineData(Result.Win)]
+        public async Task PlayAgainstComputer_correctly_relays_data_between_dependencies(Result expectedResult)
+        {
+            // Setup
+            var ct = CancellationToken.None;
+
+            shapeProvider.Setup(sp => sp.GetMaxShapeId()).Returns(MAX_SHAPE_ID);
+            shapeProvider.Setup(sp => sp.GetShape(GENERATED_SHAPE_ID)).Returns(GENERATED_SHAPE);
+            shapeProvider.Setup(sp => sp.IsValidShapeId(PLAYER_SHAPE_ID)).Returns(true);
+            randomGenerator.Setup(rg => rg.Next(1, MAX_SHAPE_ID + 1, ct))
+                           .Returns(Task.FromResult(GENERATED_SHAPE_ID));
+            gameResultCalculator.Setup(grc => grc.Calculate(PLAYER_SHAPE_ID, GENERATED_SHAPE_ID))
+                                .Returns(expectedResult);
+
+            // Execution
+            var gameRecord = await gameManager.PlayAgainstComputer(PLAYER_SHAPE_ID, ct);
+
+            // Verification
+            shapeProvider.Verify(sp => sp.IsValidShapeId(PLAYER_SHAPE_ID), Times.Once);
+            randomGenerator.Verify(rg => rg.Next(1, MAX_SHAPE_ID + 1, ct), Times.Once);
+            gameResultCalculator.Verify(grc => grc.Calculate(PLAYER_SHAPE_ID, GENERATED_SHAPE_ID), Times.Once);
+
+            Assert.Equal(expectedResult, gameRecord.Result);
+            Assert.Equal(PLAYER_SHAPE_ID, gameRecord.Player1Choice);
+            Assert.Equal(GENERATED_SHAPE_ID, gameRecord.Player2Choice);
+        }
+
+        [Fact]
+        public async Task PlayAgainstComputer_raises_exception_for_invalid_shape_id()
+        {
+            // Setup
+            var ct = CancellationToken.None;
+            shapeProvider.Setup(sp => sp.IsValidShapeId(PLAYER_SHAPE_ID)).Returns(false);
+
+            // Execution
+            var execute = async () => await gameManager.PlayAgainstComputer(PLAYER_SHAPE_ID, ct);
+
+            // Verification
+            await Assert.ThrowsAsync<ArgumentOutOfRangeException>(execute);
+        }
+    }
+}
---
+++ RPSSL.Service.Tests/GameResultCalculatorTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Logging;
+
+using Moq;
+using Xunit;
+
+using Mmicovic.RPSSL.Service.Models;
+
+namespace Mmicovic.RPSSL.Service.Tests
+{
+    public class GameResultCalculatorTests
+    {
+        private readonly IShapeProvider shapeProvider;
+        private readonly IGameResultCalculator gameResultCalculator;
+
+        public GameResultCalculatorTests()
+        {
+            shapeProvider = new ShapeProvider();
+
+            var calculatorLogger = new Mock<ILogger<GameResultCalculator>>();
+            gameResultCalculator = new GameResultCalculator(calculatorLogger.Object);
+        }
+
+        [Theory]
+        [InlineData("Rock", "Rock", Result.Tie)]
+        [InlineData("Rock", "Paper", Result.Lose)]
+        [InlineData("Rock", "Scissors", Result.Win)]
+        [InlineData("Rock", "Spock", Result.Lose)]
+        [InlineData("Rock", "Lizard", Result.Win)]
+        [InlineData("Paper", "Rock", Result.Win)]
+        [InlineData("Paper", "Paper", Result.Tie)]
+        [InlineData("Paper", "Scissors", Result.Lose)]
+        [InlineData("Paper", "Spock", Result.Win)]
+        [InlineData("Paper", "Lizard", Result.Lose)]
+        [InlineData("Scissors", "Rock", Result.Lose)]
+        [InlineData("Scissors", "Paper", Result.Win)]
+        [InlineData("Scissors", "Scissors", Result.Tie)]
+        [InlineData("Scissors", "Spock", Result.Lose)]
+        [InlineData("Scissors", "Lizard", Result.Win)]
+        [InlineData("Spock", "Rock", Result.Win)]
+        [InlineData("Spock", "Paper", Result.Lose)]
+        [InlineData("Spock", "Scissors", Result.Win)]
+        [InlineData("Spock", "Spock", Result.Tie)]
+        [InlineData("Spock", "Lizard", Result.Lose)]
+        [InlineData("Lizard", "Rock", Result.Lose)]
+        [InlineData("Lizard", "Paper", Result.Win)]
+        [InlineData("Lizard", "Scissors", Result.Lose)]
+        [InlineData("Lizard", "Spock", Result.Win)]
+        [InlineData("Lizard", "Lizard", Result.Tie)]
+        public void Calculator_correctly_calculates_game_result(string player1Shape, string player2Shape,
+                                                                Result expectedResult)
+        {
+            // Setup
+            var allShapes = shapeProvider.GetAllShapes();
+            var player1ShapeId = GetShapeId(allShapes, player1Shape);
+            var player2ShapeId = GetShapeId(allShapes, player2Shape);
+
+            // Execution
+            var result = gameResultCalculator.Calculate(player1ShapeId, player2ShapeId);
+
+            // Verification
+            Assert.Equal(expectedResult, result);
+        }
+
+        private int GetShapeId(IEnumerable<Shape> allShapes, string shapeName)
+        {
+            return allShapes.First(s => string.Equals(s.Name, shapeName, StringComparison.OrdinalIgnoreCase)).Id;
+        }
+    }
+}
