668764628f36778fc3689fc8e66bf720a0803829
Xardas97 <markomicovic19@yahoo.com>
2024-10-16 12:17:43 +0200

    Simplify validation messages

--- RPSSL.API/Validators/PostGameRecordValidator.cs
+++ RPSSL.API/Validators/PostGameRecordValidator.cs
@@ -8,19 +8,13 @@ namespace Mmicovic.RPSSL.API.Validators
     {
         public PostGameRecordValidator(Func<int, bool> isValidShapeId)
         {
-            RuleFor(c => c.PlayerChoice).NotNull()
-                                        .WithMessage("Player shape is a mandatory field");
+            RuleFor(c => c.PlayerChoice).NotNull().WithName("player");
             RuleFor(c => c.PlayerChoice).Must(shapeId => shapeId.HasValue && isValidShapeId(shapeId.Value))
                                         .WithMessage("Chosen player shape does not exist");
 
-            RuleFor(c => c.Id).Null()
-                                          .WithMessage("ID field must be empty, ID is generated");
-
-            RuleFor(c => c.Result).Null()
-                                  .WithMessage("Results field must be empty, results are calculated");
-
-            RuleFor(c => c.ComputerChoice).Null()
-                                          .WithMessage("Computer field must be empty, computer choice is randomized");
+            RuleFor(c => c.Id).Null();
+            RuleFor(c => c.Result).Null();
+            RuleFor(c => c.ComputerChoice).Null().WithName("computer");
         }
     }
 }
